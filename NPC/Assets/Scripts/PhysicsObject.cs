bfda0504be9518c45a67ec2f543451c270505a28
Jason Chen <c04jason@gmail.com>
2023-11-19 00:06:53 -0500

    Imported Aquatic package

--- NPC/Assets/Scripts/PhysicsObject.cs
+++ NPC/Assets/Scripts/PhysicsObject.cs
@@ -16,7 +16,7 @@ public class PhysicsObject : MonoBehaviour
     public float Radius { get { return radius; } }
     public Vector3 Velocity { get { return velocity; } }
     public Vector3 Direction { get { return direction; } }
-    public Vector3 Position { get { return transform.position; } set { transform.position = value; } }
+    public Vector3 Position { get { return transform.position; } }
     public Vector3 CamSize { get { return camSize; } }
 
     // Start is called before the first frame update
@@ -34,17 +34,13 @@ public class PhysicsObject : MonoBehaviour
         // Calculate the velocity for this frame
         velocity += acceleration * Time.deltaTime;
 
-        CheckBounds();
-
         transform.position += velocity * Time.deltaTime;
 
-        // Grab current direction from velocity
         if (velocity.sqrMagnitude > Mathf.Epsilon)
         {
             direction = velocity.normalized;
         }
 
-        // Zero out acceleration
         acceleration = Vector3.zero;
 
         transform.rotation = Quaternion.LookRotation(Vector3.back, direction);
@@ -53,36 +49,5 @@ public class PhysicsObject : MonoBehaviour
     public void ApplyForce(Vector3 force)
     {
         acceleration += force / mass;
-    }
-
-
-    public void ApplyFriction(float coefficient)
-    {
-        Vector3 friction = velocity * -1;
-        friction.Normalize();
-        friction *= coefficient;
-
-        ApplyForce(friction);
-    }
-
-    private void CheckBounds()
-    {
-        if (transform.position.x > camSize.x)
-        {
-            velocity.x *= -1;
-        }
-        if (transform.position.x < -camSize.x)
-        {
-            velocity.x *= -1;
-        }
-
-        if (transform.position.y > camSize.y)
-        {
-            velocity.y *= -1;
-        }
-        if (transform.position.y < -camSize.y)
-        {
-            velocity.y *= -1;
-        }
     }
 }
