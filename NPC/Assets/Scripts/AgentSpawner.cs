f466f8831ca5de2218a9fb22945a1f6c5a4525b3
Jason Chen <c04jason@gmail.com>
2023-11-19 16:26:19 -0500

    spawns

--- NPC/Assets/Scripts/AgentSpawner.cs
+++ NPC/Assets/Scripts/AgentSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,6 +23,52 @@ public class AgentSpawner : MonoBehaviour
         
     }
 
+    //private void Spawn()
+    //{
+    //    // Count of each agent type
+    //    int sharkCount = 1;
+    //    int octopusCount = 3;
+    //    int doryCount = 8;
+    //    int clownfishCount = 9;
+
+    //    // Iterate over the agentPrefabs list and spawn agents based on counts
+    //    foreach (GameObject agentPrefab in agentPrefabs)
+    //    {
+    //        int count = 0;
+
+    //        switch (agentPrefab.tag)
+    //        {
+    //            case "Shark":
+    //                count = sharkCount;
+    //                sharkCount = 0;
+    //                break;
+    //            case "Octopus":
+    //                count = octopusCount;
+    //                octopusCount = 0;
+    //                break;
+    //            case "Dory":
+    //                count = doryCount;
+    //                doryCount = 0;
+    //                break;
+    //            case "Clownfish":
+    //                count = clownfishCount;
+    //                clownfishCount = 0;
+    //                break;
+    //            default:
+    //                break;
+    //        }
+
+    //        // Spawn agents
+    //        for (int i = 0; i < count; i++)
+    //        {
+    //            Vector2 spawnPosition = new Vector2(
+    //                Random.Range(-camSize.x, camSize.x),
+    //                Random.Range(-camSize.y, camSize.y));
+    //            Instantiate(agentPrefab, spawnPosition, Quaternion.Euler(0f, 90f, 0f));
+    //        }
+    //    }
+    //}
+
     private void Spawn()
     {
         // Count of each agent type
@@ -34,24 +81,29 @@ public class AgentSpawner : MonoBehaviour
         foreach (GameObject agentPrefab in agentPrefabs)
         {
             int count = 0;
+            string sortingLayerName = "";  // Predefined sorting layer for each agent type
 
             switch (agentPrefab.tag)
             {
                 case "Shark":
                     count = sharkCount;
                     sharkCount = 0;
+                    sortingLayerName = "Farground";
                     break;
                 case "Octopus":
                     count = octopusCount;
                     octopusCount = 0;
+                    sortingLayerName = "Background";
                     break;
                 case "Dory":
                     count = doryCount;
                     doryCount = 0;
+                    sortingLayerName = "Midground";
                     break;
                 case "Clownfish":
                     count = clownfishCount;
                     clownfishCount = 0;
+                    sortingLayerName = "Foreground";
                     break;
                 default:
                     break;
@@ -60,10 +112,13 @@ public class AgentSpawner : MonoBehaviour
             // Spawn agents
             for (int i = 0; i < count; i++)
             {
-                Vector2 spawnPosition = new Vector2(
-                    Random.Range(-camSize.x, camSize.x),
-                    Random.Range(-camSize.y, camSize.y));
+                // Set the spawn position with the adjusted z value
+                Vector3 spawnPosition = new Vector3(
+                    UnityEngine.Random.Range(-camSize.x, camSize.x),
+                    UnityEngine.Random.Range(-camSize.y, camSize.y),
+                    (int)Enum.Parse(typeof(EnvLayers), sortingLayerName));
 
+                // Instantiate the agent with the adjusted z value
                 Instantiate(agentPrefab, spawnPosition, Quaternion.Euler(0f, 90f, 0f));
             }
         }
